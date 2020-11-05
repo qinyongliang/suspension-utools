b632d13516bc22a2e0e02c18108f08f31f49b9c1
hell_world <qinyongliang@hotmail.com>
2020-11-05 23:19:55 +0800

    添加截图并悬浮快捷命令

--- preload.js
+++ preload.js
@@ -1,4 +1,21 @@
 const electron = require('electron')
+
+function show(size,payload) {
+    utools.createBrowserWindow('suspend.html?a=1#' + payload, {
+        title: 'img',
+        width: size.width / (utools.isMacOs() ? 2 : 1),
+        height: size.height / (utools.isMacOs() ? 2 : 1),
+        useContentSize: true,
+        minimizable: false,
+        maximizable: false,
+        fullscreenable: false,
+        frame: false,
+        acceptFirstMouse: true,
+        alwaysOnTop: true,
+    })
+    window.utools.outPlugin() 
+}
+
 window.exports = {
     "suspend": {
         mode: "none",
@@ -10,24 +27,17 @@ window.exports = {
                     let img = electron.nativeImage.createFromPath(action.payload[0].path);
                     size = img.getSize();
                     payload = img.toDataURL();
+                    show(size, payload)
                 } else if (action.type === 'img') {
                     size = electron.nativeImage.createFromDataURL(action.payload).getSize();
                     payload = action.payload;
+                    show(size, payload)
+                } else if(action.type==='text'){
+                    utools.screenCapture(base64Str => {
+                        size = electron.nativeImage.createFromDataURL(base64Str).getSize();
+                        show(size, base64Str)
+                    })
                 }
-                
-                utools.createBrowserWindow('suspend.html?a=1#' + payload, {
-                    title: 'img',
-                    width: size.width / (utools.isMacOs() ? 2 : 1),
-                    height: size.height / (utools.isMacOs() ? 2 : 1),
-                    useContentSize: true,
-                    minimizable:false,
-                    maximizable:false,
-                    fullscreenable:false,
-                    frame: false,
-                    acceptFirstMouse:true,
-                    alwaysOnTop: true,
-                })
-                window.utools.outPlugin() 
             }
         }
     }
