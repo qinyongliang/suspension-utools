d7294e9c6865b2c2cf0cb8535f484ee01ed4f85c
qinyongliang <qinyongliang@regin.com>
2020-11-06 11:04:31 +0800

    优化缩放问题

--- preload.js
+++ preload.js
@@ -1,19 +1,25 @@
 const electron = require('electron')
 
-function show(size,payload) {
+function show(size, payload) {
     utools.createBrowserWindow('suspend.html?a=1#' + payload, {
         title: 'img',
         width: size.width / (utools.isMacOs() ? 2 : 1),
         height: size.height / (utools.isMacOs() ? 2 : 1),
         useContentSize: true,
+        //不能最大最小化
         minimizable: false,
         maximizable: false,
         fullscreenable: false,
+        //背景透明，防止放大缩小时出现白框
+        transparent: true,
+        backgroundColor: '#00000000',
         frame: false,
-        acceptFirstMouse: true,
         alwaysOnTop: true,
+        webPreferences: {
+            preload: 'suspend.js'
+        }
     })
-    window.utools.outPlugin() 
+    window.utools.outPlugin()
 }
 
 window.exports = {
@@ -27,15 +33,15 @@ window.exports = {
                     let img = electron.nativeImage.createFromPath(action.payload[0].path);
                     size = img.getSize();
                     payload = img.toDataURL();
-                    show(size, payload)
+                    show(size, payload);
                 } else if (action.type === 'img') {
                     size = electron.nativeImage.createFromDataURL(action.payload).getSize();
                     payload = action.payload;
                     show(size, payload)
-                } else if(action.type==='text'){
+                } else if (action.type === 'text') {
                     utools.screenCapture(base64Str => {
                         size = electron.nativeImage.createFromDataURL(base64Str).getSize();
-                        show(size, base64Str)
+                        show(size, base64Str);
                     })
                 }
             }
---
+++ suspend.js
@@ -0,0 +1,17 @@
+const { remote } = require('electron')
+if (remote) {
+    window.isMac = utools.isMacOs();
+    let win = remote.getCurrentWindow();
+    window.resize = (x, y) => {
+        win.setSize(x, y);
+    }
+    win.on('will-resize', (event, newBounds) => {
+        event.preventDefault();
+        //只能按比例缩放图片
+        let img = window.document.querySelector("img");
+        img.style.width = '100%';
+        let scale = img.width / (img.height * 1.0)
+        let height = newBounds.width / scale;
+        win.setSize(newBounds.width, height);
+    })
+}
