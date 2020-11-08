67061fb51e28c5cb29ae4997c98f4b1633261639
hell_world <qinyongliang@hotmail.com>
2020-11-08 18:48:40 +0800

    TODO 图片旋转

--- suspend.js
+++ suspend.js
@@ -1,21 +1,25 @@
 const { remote } = require('electron')
 if (remote) {
-    window.isMac = utools.isMacOs();
     let win = remote.getCurrentWindow();
     window.resize = (x, y) => {
         win.setSize(x, y);
     }
-    window.mv = (x, y) => {
+    window.mv = (x, y, width, height) => {
         let bound = win.getBounds();
-        bound.x += x;
-        bound.y += y;
-        win.setBounds(bound);
+        let newBounds = {
+            x: parseInt(bound.x + x),
+            y: parseInt(bound.y + y),
+            width: parseInt(width || bound.width),
+            height: parseInt(height || bound.height)
+        }
+        win.setBounds(newBounds);
     }
     win.on('will-resize', (event, newBounds) => {
         event.preventDefault();
         //只能按比例缩放图片
         let img = window.document.querySelector("img");
         img.style.width = '100%';
+        img.style.height = '100%';
         let scale = img.width / (img.height * 1.0)
         let height = newBounds.width / scale;
         win.setSize(newBounds.width, Math.min(newBounds.height, height));
